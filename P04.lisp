862973114bd6c4a7acba6a03de1cdeda9a0854c8
Deepak Venkatesh <41579969+deepak-venkatesh@users.noreply.github.com>
2025-09-26 21:15:55 -0700

    Create P04.lisp

---
+++ P04.lisp
@@ -0,0 +1,10 @@
+#|
+
+P04 (*) Find the number of elements of a list.
+
+|#
+
+;; Very simple solution. Maybe I am missing the point of these questions so far.
+
+(defun length-list (x)
+  (length x))
